0fb5977877e6dc73fd9cc78c9f5ced81bf1e29af
Erico Do Espirito Santo <esanto@brq.com>
2019-03-07 09:13:18 -0300

    Alteração

--- src/main/java/com/erico/api/PrimeiroProjetoApplication.java
+++ src/main/java/com/erico/api/PrimeiroProjetoApplication.java
@@ -8,7 +8,7 @@ public class PrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiroProjetoApplication.class, args);
-		System.out.print("Projeto");
+		System.out.println("Projeto Spring Boot");
 	}
 
 }
